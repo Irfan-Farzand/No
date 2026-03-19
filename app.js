5ed1d39a2059fb9d9a14ef7f223ff03b32c12c4f
Irfan Farzand <irfanfarzandawan@gmail.com>
2026-03-19 18:57:29 +0500

    startup

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 app.use('/api', userRoutes);
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
